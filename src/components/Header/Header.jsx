c7cecc3b2a70235a67e7b091ff6eb25546ed2c8a
Shivam-Singh0 <shivamarise68@gmail.com>
2024-07-26 12:52:02 +0530

    logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,14 +4,14 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import styles from './Header.module.css';
 import { Image } from 'react-bootstrap';
-import AlmaX_Music from '../../assets/AlmaX_Music.png';
+import amazon_music from '../../assets/amazon_music.png';
 
 function Header() {
   return (
     <Navbar expand="lg" className={`${styles.nav} px-4 py-3`} data-bs-theme="dark">
      
         <Navbar.Brand href="/">
-          <Image src={AlmaX_Music} alt="logo" width="100" height="100" className="d-inline-block align-top"  />
+          <Image src={amazon_music} alt="logo" width="100" height="100" className="d-inline-block align-top"  />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
